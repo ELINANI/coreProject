41466d6e2fb6f81975ebe4c8f758205d17bb9ad5
soufiane <elinanisoufiane@gmail.com>
2020-04-11 16:38:01 +0000

    core project

---
+++ Migrations/20200402011908_createdb1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace coreProject.Migrations
+{
+    public partial class createdb1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "adresseProfesseur",
+                table: "Professeur",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "nomProfesseur",
+                table: "Professeur",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "phoneProfesseur",
+                table: "Professeur",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "prenomProfesseur",
+                table: "Professeur",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "adresseProfesseur",
+                table: "Professeur");
+
+            migrationBuilder.DropColumn(
+                name: "nomProfesseur",
+                table: "Professeur");
+
+            migrationBuilder.DropColumn(
+                name: "phoneProfesseur",
+                table: "Professeur");
+
+            migrationBuilder.DropColumn(
+                name: "prenomProfesseur",
+                table: "Professeur");
+        }
+    }
+}
---
+++ Migrations/20200404160934_createdb22.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace coreProject.Migrations
+{
+    public partial class createdb22 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matiere_Professeur_codeFiliere",
+                table: "Matiere");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matiere_codeFiliere",
+                table: "Matiere");
+
+            migrationBuilder.DropColumn(
+                name: "codeFiliere",
+                table: "Matiere");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matiere_codeProf",
+                table: "Matiere",
+                column: "codeProf");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matiere_Professeur_codeProf",
+                table: "Matiere",
+                column: "codeProf",
+                principalTable: "Professeur",
+                principalColumn: "codeProf",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matiere_Professeur_codeProf",
+                table: "Matiere");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matiere_codeProf",
+                table: "Matiere");
+
+            migrationBuilder.AddColumn<int>(
+                name: "codeFiliere",
+                table: "Matiere",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matiere_codeFiliere",
+                table: "Matiere",
+                column: "codeFiliere");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matiere_Professeur_codeFiliere",
+                table: "Matiere",
+                column: "codeFiliere",
+                principalTable: "Professeur",
+                principalColumn: "codeProf",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
